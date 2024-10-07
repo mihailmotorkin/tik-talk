54f98ee8b161921ed70872326b115fbfc3200ea7
mihailmotorkin <motorkinmihail@gmail.com>
2024-10-07 23:09:11 +0300

    start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tick-talk';
+  
 }
