bf765885f512f4be474c2ee5fac115493653e388
Motorkin Mihail <motorkinmihail@gmail.com>
2024-10-10 17:39:55 +0300

    add svg-icon.component

---
+++ src/app/common/svg-icon/svg-icon.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'svg[icon]',
+  standalone: true,
+  imports: [],
+  template: '<svg:use [attr.href]="href"></svg:use>',
+  styles: ['']
+})
+export class SvgIconComponent {
+  @Input() icon: string = '';
+
+  get href() {
+    return `/assets/${this.icon}.svg#${this.icon}`;
+  }
+
+}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
@@ -14,6 +14,8 @@ export class LoginPageComponent {
   authService = inject(AuthService);
   router = inject(Router);
 
+  isPasswordVisible = signal<boolean>(false);
+
   form = new FormGroup({
     username: new FormControl<string | null>(null, Validators.required),
     password: new FormControl<string | null>(null, Validators.required)
@@ -23,7 +25,7 @@ export class LoginPageComponent {
     if (this.form.valid) {
       //@ts-ignore
       this.authService.login(this.form.value)
-        .subscribe(res => {
+        .subscribe(() => {
           this.router.navigate(['']);
         });
     }
