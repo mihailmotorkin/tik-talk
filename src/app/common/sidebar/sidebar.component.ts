b83efcacdfd2f12f93e65e397f3b500da028f019
mihailmotorkin <motorkinmihail@gmail.com>
2024-10-14 20:58:26 +0300

    add subscriber-card

--- src/app/common/sidebar/sidebar.component.ts
+++ src/app/common/sidebar/sidebar.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { CommonModule } from '@angular/common';
+import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, CommonModule, SubscriberCardComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chat',
+      link: 'chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    }
+  ]
 }
