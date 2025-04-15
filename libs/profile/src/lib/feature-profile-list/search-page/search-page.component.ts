38347199b117b14860de2070e9bfe270b9bfa629
mihailmotorkin <motorkinmihail@gmail.com>
2025-04-15 18:31:03 +0300

    infinite scroll

--- libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
@@ -8,12 +8,13 @@ import {
 import { ProfileCardComponent } from '../../ui';
 import { ProfileFiltersComponent } from '../profile-filters/profile-filters.component';
 import { debounceTime, fromEvent } from 'rxjs';
-import { selectFilteredProfiles } from '../../data';
+import { profileActions, selectFilteredProfiles } from '../../data';
 import { Store } from '@ngrx/store';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-search-page',
-  imports: [ProfileCardComponent, ProfileFiltersComponent],
+  imports: [ProfileCardComponent, ProfileFiltersComponent, InfiniteScrollDirective],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
@@ -36,5 +37,21 @@ export class SearchPageComponent implements AfterViewInit {
     const height = window.innerHeight - top - 24;
 
     this.r2.setStyle(this.hostElement.nativeElement, 'height', `${height}px`);
+  }
+
+  timeToFetch() {
+    this.store.dispatch(profileActions.setPage({}))
+  }
+
+  // onIntersection(entries: IntersectionObserverEntry[]) {
+  //   if (!entries.length) { return; }
+  //
+  //   if (entries[0].intersectionRatio > 0) {
+  //     this.timeToFetch();
+  //   }
+  // }
+
+  onScroll() {
+    this.timeToFetch()
   }
 }
